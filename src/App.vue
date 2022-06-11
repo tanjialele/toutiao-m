a99c627696d660732da7cd00405632e4bf7cc2eb
“tanjiale” <1696367211@qq.com>
2022-06-11 16:01:57 +0800

    把头条放进去

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+<!-- 路由出口 -->
+    <router-view/>
+    <h1>我是美女</h1>
+    
+  </div>
+</template>
+<script>
+export default {
+  // 建议给每个组件都取一个名字
+  name: 'APP'
+}
+</script>
+
+<style lang="less">
+
+</style>
